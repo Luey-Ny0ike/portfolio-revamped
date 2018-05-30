de05b3b02ee069403609215c0f86f04aa7ba7533
Lewis Nyoike <nyoikelewis@gmail.com>
2018-05-30 16:11:11 +0300

    Added the attachement to the project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+  has_attached_file :cover_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
+ validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\z/
 end
