6db340fe56e1717e696eb5928b1f8789e4a0eb6f
Lewis Nyoike <nyoikelewis@gmail.com>
2018-05-30 16:42:56 +0300

    Associatio between category and project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  belongs_to :category
   has_attached_file :cover_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
  validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\z/
 end
