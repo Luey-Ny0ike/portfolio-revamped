043a7c988c83ae418a81dc575e051dcf8ffe8e96
Lewis Nyoike <nyoikelewis@gmail.com>
2018-05-30 16:11:03 +0300

    Added the attachement to the blog model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ApplicationRecord
+  has_attached_file :blog_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+ validates_attachment_content_type :blog_photo, content_type: /\Aimage\/.*\z/
 end
