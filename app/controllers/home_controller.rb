082eb277e9d8958edbec6e96f611db9a8904ee70
Lewis Nyoike <nyoikelewis@gmail.com>
2018-06-05 16:07:15 +0300

    Retriced the skills and projects accordingly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def index
+    @categories = Category.all
+    @web_design = Category.find_by(name: "Web design")
+    @videography = Category.find_by(name: "Videography")
+    @web_designs = Project.where(category_id: @web_design.id)
+    @video_projects = Project.where(category_id: @videography.id)
+  end
 end
