d5e965c6df36b9fed0db559033960cb57eb569cd
Lewis Nyoike <nyoikelewis@gmail.com>
2018-06-05 16:08:02 +0300

    Added the large option in image uploads

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :category
-  has_attached_file :cover_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
+  has_attached_file :cover_photo, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
  validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\z/
 end
