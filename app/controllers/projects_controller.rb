377bec203df6af7e2a2b10bd13d2c5d56f0d78f8
Lewis Nyoike <nyoikelewis@gmail.com>
2018-06-07 13:52:08 +0300

    Permitted the extra param

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -75,6 +75,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :challenge, :link, :description, :cover_photo, :category_id)
+      params.require(:project).permit(:title, :challenge, :link, :description, :cover_photo, :category_id, :slogan)
     end
 end
