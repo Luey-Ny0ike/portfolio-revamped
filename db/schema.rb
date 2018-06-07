30ec5c2604a847be950ca19d4ce6b4b29ac7b908
Lewis Nyoike <nyoikelewis@gmail.com>
2018-06-07 04:02:50 +0300

    MIgrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530161540) do
+ActiveRecord::Schema.define(version: 20180607000514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,13 @@ ActiveRecord::Schema.define(version: 20180530161540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.string "progress"
+    t.string "color"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.integer "rating"
   end
 
   create_table "users", force: :cascade do |t|
