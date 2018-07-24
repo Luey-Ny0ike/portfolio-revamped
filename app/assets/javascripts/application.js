7fb92f4aadd7e43e3935148f2ab886f8c77f4936
lewis nyoike <nyoikelewis@gmail.com>
2018-07-24 21:21:08 +0300

    Js for the navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,31 @@
 //= require_tree .
 
 $(document).ready(function(){
-  
+  var container = document.querySelector('.container-nav');
+var main = document.querySelector('.main');
+
+function toggleSidebar(){
+    isShowingSidebar() ? hideSidebar() : showSidebar();
+}
+
+function showSidebar(){
+    container.classList.add('show-sidebar');
+}
+
+function hideSidebar(){
+    container.classList.remove('show-sidebar');
+}
+
+function isShowingSidebar(){
+    return container.classList.contains('show-sidebar');
+}
+
+document.querySelector('.hamburger').addEventListener('click', toggleSidebar, false);
+
+container.addEventListener('click', function(e){
+    if(isShowingSidebar() && main.contains(e.target)){
+        e.preventDefault();
+        hideSidebar();
+    }
+}, true);
 })
