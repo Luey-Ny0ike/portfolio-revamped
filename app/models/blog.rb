7411c3831534640846951b5d3c8d98ed46e77bd0
lewis nyoike <nyoikelewis@gmail.com>
2018-08-18 22:46:58 +0300

    Added an extra image resize option

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
-  has_attached_file :blog_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  belongs_to :blog_category
+  has_attached_file :blog_photo, styles: { background: "1920x1116>",large: "700x700>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
  validates_attachment_content_type :blog_photo, content_type: /\Aimage\/.*\z/
 end
